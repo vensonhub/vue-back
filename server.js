764a9b880478845058e4f269651d9b32421f42a5
jiajia <283747923@qq.com>
2019-10-16 02:07:11 +0800

    init

---
+++ server.js
@@ -0,0 +1,23 @@
+let http = require('http');
+let users = [
+  {id:1,name:'data1'},
+  {id:2,name:'data2'},
+  {id:3,name:'data3'},
+];
+let server = http.createServer((req, res)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  if(req.url === '/api/users') {
+    res.end(JSON.stringify(users));
+  } else {
+    res.end('Not Found');
+  }
+  if(req.method.toLowerCase() === 'options') {
+    res.end();
+  }
+});
+server.listen(3000,()=>{
+  console.log('server start at port 3000');
+});
