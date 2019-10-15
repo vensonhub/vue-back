bdbf5f7e5b50146ba4aad6d02873bf182db56307
jiajia <283747923@qq.com>
2019-10-16 05:27:59 +0800

    change 6

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http');
 let users = [
-  {id:1,name:'data1'},
-  {id:2,name:'data2'},
-  {id:3,name:'data3'},
+  {id:1,name:'data11'},
+  {id:2,name:'data22'},
+  {id:3,name:'data33'},
 ];
 let server = http.createServer((req, res)=>{
   res.setHeader('Access-Control-Allow-Origin','*');
